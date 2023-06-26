ba77ba25c6e98739427aaabcff8cae0303a5a715
mehedihasan8 <mehedihasanfoysal22@gmail.com>
2023-06-26 13:45:01 +0600

    Added Our Job Category

--- src/Components/JobsCard.jsx
+++ src/Components/JobsCard.jsx
@@ -1,13 +1,13 @@
 const JobsCard = ({ category }) => {
   return (
-    <div className="bg-gray-100 shadow-lg rounded-lg p-8">
+    <div className="bg-[#E2E8F0] shadow-lg rounded-lg p-8">
       <img
         className="w-12 bg-gray-300 mt-3 p-2 rounded-md"
         src={category.img}
         alt=""
       />
       <h1 className="text-xl font-bold my-4">{category.title}</h1>
-      <h2 className="font-semibold py-4">
+      <h2 className="font-semibold py-3">
         {category.availableNumber}+ Jobs Available
       </h2>
     </div>
